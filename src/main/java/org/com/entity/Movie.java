ecf2a5bd725688e683ef389f957d7a2925126043
Alison8547 <alisonailson100@hotmail.com>
2023-09-22 14:21:25 -0300

    feat: Add lombok and entity movie

---
+++ src/main/java/org/com/entity/Movie.java
@@ -0,0 +1,14 @@
+package org.com.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    private Integer id;
+    private String title;
+}
