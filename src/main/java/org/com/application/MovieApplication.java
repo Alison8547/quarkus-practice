50f87f8a1ef0596f66f5f384b07862454197c22d
Alison8547 <alisonailson100@hotmail.com>
2023-09-23 11:39:23 -0300

    feat: @OpenAPIDefinition for class MovieApplication

---
+++ src/main/java/org/com/application/MovieApplication.java
@@ -0,0 +1,24 @@
+package org.com.application;
+
+import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Movie API",
+                description = "Movie application",
+                version = "1.0.0",
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html")
+        )
+        ,
+        tags = {
+                @Tag(name = "movies", description = "Movies")
+        }
+)
+public class MovieApplication extends Application {
+}
